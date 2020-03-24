2dd420bad5612f6ced9a012a47d323164b27ed8c
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-03-24 22:59:13 +0100

    wip

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+
+import reducer from './redux/reducers';
+import RepoList from './RepoList';
+
+const client = axios.create({
+  baseURL: 'https://api.github.com',
+  responseType: 'json'
+});
+
+const store = createStore(reducer, applyMiddleware(axiosMiddleware(client)));
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+            <RepoList />
+        </View>
+      </Provider>
   );
 }
 
