1cdd6fbd7848e9ea7821364ad05bbddd472cd0f0
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-03-24 22:36:28 +0100

    reducer and action types

---
+++ redux/actionTypes.js
@@ -0,0 +1,3 @@
+export const GET_REPOS = 'react-native-redux/repos/LOAD';
+export const GET_REPOS_SUCCESS = 'react-native-redux/repos/LOAD_SUCCESS';
+export const GET_REPOS_FAIL = 'react-native-redux/repos/LOAD_FAIL';
